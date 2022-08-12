9340478aa57c8d487f70f3804cc30c2ed0f3b69b
DohyungKim <ehgud456456@naver.com>
2022-08-13 00:48:29 +0900

    add react-redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages";
+import Home from "./pages/index";
 import Intro from "./pages/Intro";
 
 const App = () => (
