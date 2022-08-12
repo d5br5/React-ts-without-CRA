7208533c05d589986c65921cfe602c40b27db12d
DohyungKim <ehgud456456@naver.com>
2022-08-13 01:46:51 +0900

    add redux-saga

--- src/modules/counter.ts
+++ src/modules/counter.ts
@@ -1,5 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
+import { call, delay, put, takeLatest } from "redux-saga/effects";
 
 export interface CounterState {
 	value: number;
@@ -22,8 +23,36 @@ export const counterSlice = createSlice({
 		incrementByAmount: (state, action: PayloadAction<number>) => {
 			state.value += action.payload;
 		},
+		incrementSuccess: (state) => {
+			state.value = 1000;
+		},
+		incrementFailure: (state) => {
+			state.value = -400;
+		},
 	},
 });
 
-export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+function* incrementSaga(action: PayloadAction<CounterState>) {
+	try {
+		yield call(increment);
+		yield delay(1000);
+		yield put({
+			type: incrementSuccess,
+		});
+	} catch (err) {
+		yield put({ type: incrementFailure });
+	}
+}
+
+export function* watchIncrement() {
+	yield takeLatest(increment.type, incrementSaga);
+}
+
+export const {
+	increment,
+	decrement,
+	incrementByAmount,
+	incrementSuccess,
+	incrementFailure,
+} = counterSlice.actions;
 export default counterSlice.reducer;
--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,9 +1,21 @@
+import createSagaMiddleware from "@redux-saga/core";
 import { configureStore } from "@reduxjs/toolkit";
+import { all, fork } from "redux-saga/effects";
 import counterReducer from "./counter";
+import { watchIncrement } from "./counter";
+
+const sagaMiddleware = createSagaMiddleware();
+
+function* saga() {
+	yield all([fork(watchIncrement)]);
+}
 
 export const store = configureStore({
 	reducer: { counter: counterReducer },
+	middleware: [sagaMiddleware],
 });
+
+sagaMiddleware.run(saga);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
